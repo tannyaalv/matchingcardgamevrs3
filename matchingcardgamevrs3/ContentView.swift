c0760912b146944978863ffb9958220ee23f71c1
Tanya Alvarez-Salsedo <tanyaalvsal@tamu.edu>
2024-04-24 14:35:04 -0500

    Initial Commit

---
+++ matchingcardgamevrs3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  matchingcardgamevrs3
+//
+//  Created by Alvarez-Salsedo, Tanya on 4/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ matchingcardgamevrs3/matchingcardgamevrs3App.swift
@@ -0,0 +1,17 @@
+//
+//  matchingcardgamevrs3App.swift
+//  matchingcardgamevrs3
+//
+//  Created by Alvarez-Salsedo, Tanya on 4/24/24.
+//
+
+import SwiftUI
+
+@main
+struct matchingcardgamevrs3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
